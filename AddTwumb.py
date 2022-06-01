f92fac20b3309cf9161730084a74d15c8ac65844
Paulz25 <paulz25@greenhill.org>
2022-06-01 09:37:52 -0500

    new one

---
+++ AddTwumb.py
@@ -0,0 +1,18 @@
+#Zara E Paul
+#Tw numbers 
+# We wil ask fr tw numbers  
+# and add them
+# Any thing after # is 
+# ignored by the computer 
+# import things
+import os
+os.system('clear')
+#Use variables 
+Num1=23
+Num2=12
+answer=Num1+Num2
+#Operators +, -, *,/
+print(Num1+Num2)
+print(Num1) #after each print it makes a return 
+print(Num2, end= '/ ') #change end to space 
+print (answer)
