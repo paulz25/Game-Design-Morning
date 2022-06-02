c3943ad0805ba418f633f2e3793f5486ebe8560a
Paulz25 <paulz25@greenhill.org>
2022-06-02 16:06:58 -0500

    new

---
+++ james.py
@@ -0,0 +1,28 @@
+#Zara Paul
+#This is a string practice 
+import os
+os.system('clear')
+#Create a string made of the first, middle and last character
+message= "James"
+print(message [0],end= '')
+print(message [2],end= '')
+print(message [4])
+#Write a program to create a new string made of the middle three characters of an input string.
+#JhonDipPeta
+#want Dip
+message= "JhonDipPeta"
+middle= int(len(message)/2)
+print(middle)
+print(message [4],end= '')
+print(message [5],end= '')
+print(message [6])
+#JaSonAy output Son
+message= "JaSonAy"
+print(message [2],end= '')
+print(message [3],end= '')
+print(message [4])
+
+
+
+
+
