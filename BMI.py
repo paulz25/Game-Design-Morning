0bb88801c7be4a291054c528de6e5d9ad2223e3c
Paulz25 <paulz25@greenhill.org>
2022-06-02 16:07:07 -0500

    new

--- BMI.py
+++ BMI.py
@@ -1,6 +1,9 @@
-import os
-os. system ('clear')
-x = 60 #inputed weight 
-y = 1.75 #inputed height 
-BMI = x/(y*y) #height squared
-print (BMI)
+#zara paul 
+#BMI Code 
+import os # makes everything look nicer 
+os. system ('clear') #you always put after import os 
+x = 60 #inputed weight in kg
+y = 1.75 #inputed height in meters
+BMI = x/(y*y) #height squared times weight 
+print (BMI) #final answer 
+
