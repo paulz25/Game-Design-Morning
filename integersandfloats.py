f7c34c71071611f65733fe0b68e9ba2c34690d33
Paulz25 <paulz25@greenhill.org>
2022-06-03 09:07:34 -0500

    new

--- integersandfloats.py
+++ integersandfloats.py
@@ -6,21 +6,18 @@ import os
 os .system('clear')
 
 variable= input('enter a number') #put number in 
-variable=int(variable) #number is integer 
-num= (variable % 2) #% modulu thing 
+num=int(variable) #number is integer 
 print(num) #prints the 1 or the 0 
-if num == 0: 
+if num%2 == 0: 
     print ('you are even') 
-else:
+else: 
     print('you are odd') 
 
 
 if (num%3==0):
     print ("number is a multiple of 3" )
-else:
-    print ("number is not a multiple of 3")
+
 
 if (num%5==0):
     print ("number is a multiple of 5" )
-else:
-    print ("number is not a multiple of 5") 
+
