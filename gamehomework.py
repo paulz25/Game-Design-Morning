a36aff3ac42e29d8748f219d5cac580cd54d316b
Paulz25 <paulz25@greenhill.org>
2022-06-05 18:49:23 -0500

    new

--- gamehomework.py
+++ gamehomework.py
@@ -48,152 +48,41 @@ print("5) But, you only have 5 tries to guess.....")
 print("---------------------------------------------------------------------")
 print("                      Will you succeed?")
 print("**********************************************************************")
-print("                    ")
-print("                    ")
-print("                    ")
-print("                    ")
+print()
+print()
+print()
+print()
 
 
 thislist= ["Pink", "Pink", "Pink", "Pink", "Pink", "Pink", "Pink"] #do 10 colors 
 word= random.choice(thislist) #chooses complete random element
 loop=False  
 
-# for(i)in range (5):
-
-
-
-
-
-print("                            ")
-print("---------------------------------------------------------------------")
-print("                            GUESS #1")
-print("---------------------------------------------------------------------")
-print("                            ")
-guess = input ("Input a guess:") 
-
-print("                            ")
-print("**********************************************************************")
-print("                            ")
-if guess.lower() == word.lower(): #is this correct 
-    print( "Answer is correct!")
-    print("                            ")
-    print("CONGRATS!!! YOU DID IT!!!")
-    print("                            ")
-    print("Play Again?")
-    print("                            ")
-    print("**********************************************************************")
-else:
-    print ("Sorry, you didn't guess the word. Guess again!")
-    print("                            ")
-    print("**********************************************************************")
-    print("                            ")
-    print("Hint: This word is a color")
-    print("                            ")
+for(i)in range (5): 
+    print()
     print("---------------------------------------------------------------------")
-    print("                            GUESS #2")
+    print("                            GUESS #", i+1)
     print("---------------------------------------------------------------------")
-    print("                            ")
-    hi= input("Guess any word here:")
+    print()
+    guess = input ("Input a color:") 
 
-    print("                            ")
+    print()
     print("**********************************************************************")
-    print("                            ")
-
-    if hi is word:
+    print()
+    if guess.lower() == word.lower(): #is this correct 
         print( "Answer is correct!")
-        print("                            ")
+        print()
         print("CONGRATS!!! YOU DID IT!!!")
-        print("                            ")
-        print("Play Again?")
-        print("                            ")
+        print()
+        print("**********************************************************************")
+        break
+    elif i==4:
+        print("The word was", word)
+        print()
         print("**********************************************************************")
     else:
         print ("Sorry, you didn't guess the word. Guess again!")
-        print("                            ")
+        print()
         print("**********************************************************************")
-        print("                            ")
-        print("Hint: This word is a color") #if word is blue print..... #how do you make it until user does not want to play anymore, send email and ask if mine is okay 
-
-        print("                            ")
-        print("---------------------------------------------------------------------")
-        print("                            GUESS #3")
-        print("---------------------------------------------------------------------")
-        print("                            ")
-        hi= input("Guess any word here:")
-
-        print("                            ")
-        print("**********************************************************************")
-        print("                            ")
-
-        if hi is word:
-            print( "Answer is correct!")
-            print("                            ")
-            print("CONGRATS!!! YOU DID IT!!!")
-            print("                            ")
-            print("Play Again?")
-            print("                            ")
-            print("**********************************************************************")
-        else:
-            print ("Sorry, you didn't guess the word. Guess again!")
-            print("                            ")
-            print("**********************************************************************")
-            print("                            ")
-            print("Hint: This word is a color")
-
-            print("                            ")
-            print("---------------------------------------------------------------------")
-            print("                            GUESS #4")
-            print("---------------------------------------------------------------------")
-            print("                            ")
-            hi= input("Guess any word here:")
-
-            print("                            ")
-            print("**********************************************************************")
-            print("                            ")
-
-            if hi is word:
-                print( "Answer is correct!")
-                print("                            ")
-                print("CONGRATS!!! YOU DID IT!!!")
-                print("                            ")
-                print("Play Again?")
-                print("                            ")
-                print("**********************************************************************")
-            else:
-                print ("Sorry, you didn't guess the word. Guess again!")
-                print("                            ")
-                print("**********************************************************************")
-                print("                            ")
-                print("Hint: This word is a color")
-                print("                            ")
-                print("---------------------------------------------------------------------")
-                print("                            GUESS #5")
-                print("---------------------------------------------------------------------")
-                print("                            ")
-
-            hi= input("Guess any word here:")
-
-            print("                            ")
-            print("**********************************************************************")
-            print("                            ")
-
-            if hi is word:
-                print( "Answer is correct!")
-                print("                            ")
-                print("CONGRATS!!! YOU DID IT!!!")
-                print("                            ")
-                print("Play Again?")
-                print("                            ")
-                print("**********************************************************************")
-                
-            else:
-                print ("Sorry, you didn't guess the word!")
-                print("                            ")
-                print ("You lost the game.. try again next time")
-                print("                            ")
-                print("**********************************************************************")
-                print("                            ")
-                print("The word was:")
-                print (word)
-                print("                            ")
-                print("**********************************************************************")
+        print()
+        
