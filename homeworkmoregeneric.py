6bc0c130e18896af9e4abf9b05492c72f491b312
Paulz25 <paulz25@greenhill.org>
2022-06-07 08:52:51 -0500

    new

--- homeworkmoregeneric.py
+++ homeworkmoregeneric.py
@@ -34,9 +34,9 @@ while Game:
     print("|***************************************|")
     print("|         Guess The Animal!!!           |")
     print("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|")
-    print("| 1. Animals                            |")
+    print("| 1. Colors                             |")
     print("| 2. Fruits                             |")
-    print("| 3. Computer Parts                     |")
+    print("| 3. Animals                            |")
     print("|First we will provide you with one hint|")
     print("|           !Your Hint Is!              |")
     print("|  These animals are big fans of water  |")
@@ -62,16 +62,87 @@ while Game:
         except:
             print("Plese enter a number")
 
-    os.system('cls')
-    check = True
+def option1():
+    global count 
+    os.system("clear")
+    check= True
+    count= 0 
+    list= ["Pink", "Blue", "Red", "Green", "Orange", "Yellow", "Purple", "Magenta", "Gray","Black"]
+    theword=random.choice(list)
     while check and count <5:
-        guess=input("plese put your guess here: ")
+        guess=input("Put Guess here")
         if guess == theword:
-            print("Congrats, You got it")
-            check = False
+            print("congrats, you got it")
+            check = False 
         else:
-            hint()
+            print("you failed!!!")
+            print("             *                    *  ")
+            print()
+            print("                      . . .")          
+            print('                     .      .')                                 #I TRIED TO MAKE A BIG SAD FACE!!!! 
+            print('                   .         .') 
+            print('                 .             .') 
+            print('                .               .')
+            print('               .                 .')
+            print('              .                   .' )
+            print('             .                     .')
+            print("            .                       .")
         count += 1
+
+    def option2():
+        global count 
+    os.system("clear")
+    check= True
+    count= 0 
+    list= ["Pig", "Dog", "Cat", "Bird", "Fish", "Cow", "Penguin", "Monkey", "Flamingo","Giraffe"]
+    theword=random.choice(list)
+    while check and count <5:
+        guess=input("Put Guess here")
+        if guess == theword:
+            print("congrats, you got it")
+            check = False 
+        else:
+            print("you failed!!!")
+            print("             *                    *  ")
+            print()
+            print("                      . . .")          
+            print('                     .      .')                                 #I TRIED TO MAKE A BIG SAD FACE!!!! 
+            print('                   .         .') 
+            print('                 .             .') 
+            print('                .               .')
+            print('               .                 .')
+            print('              .                   .' )
+            print('             .                     .')
+            print("            .                       .")
+        count += 1
+
+def option3():
+    global count 
+    os.system("clear")
+    check= True
+    count= 0 
+    list= ["Apple", "Orange", "Bannana", "Blueberry", "Strawberry", "Blackberry", "Raspberry", "Cherry", "Watermelon","Mango"]
+    theword=random.choice(list)
+    while check and count <5:
+        guess=input("Put Guess here")
+        if guess == theword:
+            print("congrats, you got it")
+            check = False 
+        else:
+            print("you failed!!!")
+            print("             *                    *  ")
+            print()
+            print("                      . . .")          
+            print('                     .      .')                                 #I TRIED TO MAKE A BIG SAD FACE!!!! 
+            print('                   .         .') 
+            print('                 .             .') 
+            print('                .               .')
+            print('               .                 .')
+            print('              .                   .' )
+            print('             .                     .')
+            print("            .                       .")
+        count += 1
+        
 
     os.system('cls')
     answer = input("Do you want to play again? ")
