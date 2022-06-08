57d6a3399fb257e5a05fbc4f0cf53b02860e4168
Paulz25 <paulz25@greenhill.org>
2022-06-08 01:10:24 -0500

    new

--- gamehomeworkmodified.py
+++ gamehomeworkmodified.py
@@ -26,35 +26,35 @@
 #End
 
 import random #For random generator 
-import os
-os.system('clear')
-from secrets import choice
-from tabnanny import check 
-from time import sleep
+import os #importing all the key words 
+os.system('clear') #importing all the key words 
+from secrets import choice #importing all the key words 
+from tabnanny import check #importing all the key words 
+from time import sleep #importing all the key words 
 #Print menu options 
 
 
 
-os.system ('clear')
-seconds=.5
+os.system ('clear') #makes look clean
+seconds=.5 
 
-count = 0
-Game = True
-theword = ""
+count = 0 
+Game = True 
+theword = "" #is like an import thing for the word, this is what user inputs 
 
-def option1():
-    global count 
-    os.system("clear")
-    check= True
+def option1(): #option 1 is our first option we have on the menu 
+    global count #this makes sure it runs to all the code 
+    os.system("clear") #makes look clean 
+    check= True #starts the loop/guesses 
     count= 0 
     list= ["Pink", "Blue", "Red", "Green", "Orange", "Yellow", "Purple", "Magenta", "Gray","Black"]
-    theword=random.choice(list)
-    while check and count <5:
-        guess=input("Input name of a color:")
-        if guess == theword:
-            print("congrats, you got it")
-            check = False 
-        else:
+    theword=random.choice(list) #imports random 
+    while check and count <5: #makes look run thourgh 5 times
+        guess=input("Input name of a color:") #for the user to put in the name of something 
+        if guess == theword: 
+            print("congrats, you got it") #when they get it right 
+            check = False  #breaks game if right 
+        else: #if wrong 
             print("you failed!!!")
             print("             *                    *  ")
             print()
@@ -67,17 +67,17 @@ def option1():
             print('              .                   .' )
             print('             .                     .')
             print("            .                       .")
-        count += 1
+        count += 1 #happens once in the loop 
 
-def option2():
+def option2(): #comments for all the options will be the same besides the different lists 
     global count 
-    os.system("clear")
+    os.system("clear") #makes look clean
     check= True
     count= 0 
-    list= ["Pig", "Dog", "Cat", "Bird", "Fish", "Cow", "Penguin", "Monkey", "Flamingo","Giraffe"]
+    list= ["Pig", "Dog", "Cat", "Bird", "Fish", "Cow", "Penguin", "Monkey", "Flamingo","Giraffe"] #aniamls for option 2
     theword=random.choice(list)
     while check and count <5:
-        guess=input("Input name of animal: ")
+        guess=input("Input name of animal: ") #reminds user what they are guessing 
         if guess == theword:
             print("congrats, you got it")
             check = False 
@@ -96,15 +96,15 @@ def option2():
             print("            .                       .")
         count += 1
 
-def option3():
+def option3(): #comments for all the options will be the same besides the different lists 
     global count 
-    os.system("clear")
+    os.system("clear") #makes look clean
     check= True
     count= 0 
-    list= ["Apple", "Orange", "Bannana", "Blueberry", "Strawberry", "Blackberry", "Raspberry", "Cherry", "Watermelon","Mango"]
+    list= ["Apple", "Orange", "Bannana", "Blueberry", "Strawberry", "Blackberry", "Raspberry", "Cherry", "Watermelon","Mango"] #fruits fro option 3
     theword=random.choice(list)
     while check and count <5:
-        guess=input("Input name of a fruit:")
+        guess=input("Input name of a fruit:") #reminds user what they are guesisng 
         if guess == theword:
             print("congrats, you got it")
             check = False 
@@ -125,8 +125,8 @@ def option3():
 
 
 
-while Game: 
-    os.system('clear')
+while Game: #while loop makes sure loop runs and runs and runs 
+    os.system('clear') #makes look clean
     print("**********************************************************************") #Making it pretty 
     print("----------------------------------------------------------------------")#Making it pretty 
     print("                      Guess The Word Game!")
@@ -134,7 +134,7 @@ while Game:
     print("**********************************************************************") #Making it pretty 
     print("FIRST SELECT GAME MODE")
     print("1) Colors")
-    print("2) Fruits")
+    print("2) Fruits") #menu 
     print("3) Animals")
     print("----------------------------------------------------------------------")#Making it pretty 
     print("***************************INSTRUCTIONS*******************************")#Making it pretty 
@@ -155,36 +155,36 @@ while Game:
     print()
     print()
     print()
+# we put below because if not while loop would try to apply to all 
 
+    name = input("What is your name? ") #making it cute and personal 
+    print(name, end = ", ") #makes sure that the name has commas and stuff 
+    answer = input("would you like to play the game? ") #asking user 
+    answer = answer.lower() #makes sure lowercase is usable
+    while 'n' in answer: #no answer 
+        Game = False #stops game 
+        break #ends code if user doesn't want to play 
 
-    name = input("What is your name? ")
-    print(name, end = ", ")
-    answer = input("would you like to play the game? ")
-    answer = answer.lower()
-    while 'n' in answer:
-        Game = False
-        break
-
-    while True:
-        choice = input("What game would you like to play 1,2, or 3? ")
+    while True: #when the user answers yes:
+        choice = input("What game would you like to play 1,2, or 3? ") #asking what game 
         try:
-            choice = int(choice)
+            choice = int(choice) #makes sure choice is a whole number 
             if choice > 0 and choice < 4:
-                break
+                break #code wont run if this and will print whats on line 175, if integers are within range, it will print 
             else:
                 print("give me 1,2, or 3")
-        except:
+        except: #like an elif statement 
             print("Plese enter a number")
     if choice == 1:
-        option1()
+        option1() #if users choose option __ then they will recieve that option 
     if choice == 2:
         option2()
     if choice == 3:
-        option3()
+        option3() #functions need these thinsgs 
         
-    answer = input("Do you want to play again? ") #
+    answer = input("Do you want to play again? ") #asks user 
     if ('n' or 'N') in answer:
-        Game = False
+        Game = False #will stop and print if user says no 
         print("Thank you for playing")
 
 
